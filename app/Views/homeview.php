77dd9dc79e9d56cd030ac6cce6398a486f4913ab
shubhth <godfreymatagaro@gmail.com>
2024-07-21 09:15:53 +0300

    what we do

--- app/Views/homeview.php
+++ app/Views/homeview.php
@@ -4,4 +4,5 @@
 <?= $this->include('partials/_hero') ?>
 <?= $this->include('partials/_highlights') ?>
 <?= $this->include('partials/_testimonials') ?>
+<?= $this->include('partials/_what-we-do') ?>
 <?= $this->endSection() ?>
---
+++ app/Views/partials/_what-we-do.php
@@ -0,0 +1,15 @@
+<section class="what-we-do">
+    <h2 class="section-title">What We Do</h2>
+    <div class="blue-rectangle">
+        <div class="left-div">
+            <img src="<?= base_url('public/images/smiling-lady.png');?>" alt="Description of Image" class="rectangle-image">
+        </div>
+        <div class="right-div">
+            <p>Explore and compare UK and India biometric data privacy laws through interactive maps, timelines, and searchable databases.</p>
+            <div class="buttons">
+                <button class="btn get-started">Get Started</button>
+                <button class="btn resources">Resources</button>
+            </div>
+        </div>
+    </div>
+</section>
