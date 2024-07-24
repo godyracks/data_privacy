c21b5a387e6a38f6f549cdec807619869d57c3b2
shubhth <godfreymatagaro@gmail.com>
2024-07-24 20:40:55 +0300

    profileview

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -16,6 +16,7 @@ $routes->get('contact', 'Contact::index');
 $routes->get('auth', 'Login::index');
 $routes->get('/google-login', 'GoogleAuthController::login');
 $routes->get('/google-callback', 'GoogleAuthController::callback');
+$routes->get('/profile', 'Profile::index');
 $routes->get('terms-and-conditions', 'Terms::index');
 $routes->get('privacy-policy', 'PrivacyPolicy::index');
 $routes->get('/dashboard', 'Dashboard::index');
